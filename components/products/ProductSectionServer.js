fd1d79c3ad21af10c43a7c413621337b1040e84d
Neeraj Yadav <neeraj95578@gmail.com>
2025-03-26 19:18:12 +0530

    updates

--- components/products/ProductSectionServer.js
+++ components/products/ProductSectionServer.js
@@ -1,6 +1,5 @@
 // components/products/ProductSectionServer.jsx
 import ProductSection from './ProductSection';
-import productData from './ProductData';
 
 export default function ProductSectionServer() {
   return <ProductSection />;
